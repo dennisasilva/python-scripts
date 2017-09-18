0bcc2f664efd61f1638979162bf9bc3a1fb13553
Dennis Silva <dennis.a.silva@gmail.com>
2017-09-18 01:15:47 -0300

    Update smtpenumv2.py

--- smtpenumv2.py
+++ smtpenumv2.py
@@ -5,10 +5,12 @@ import sys
 import re
 
 file = open('users.txt')
+# To read lines in python script you should use the readliness.
 for linha in file.readliness():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((sys.argv[1],587))
 	s.send("VRFY "+linha)
 	result = s.recv(1024)
 	if.re.search('252',result):
+#To filter some information you should use the strip 
 		print "Usuario encontrado: "+result.strip('252 2.0.0')
